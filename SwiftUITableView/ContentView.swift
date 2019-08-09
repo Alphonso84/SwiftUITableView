64ab46bb443f3aced1e4d439a8b4a08dba439ef1
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2019-08-09 16:59:52 -0700

    commit1

--- SwiftUITableView/ContentView.swift
+++ SwiftUITableView/ContentView.swift
@@ -8,9 +8,26 @@
 
 import SwiftUI
 
+struct Person: Identifiable {
+    var id = UUID()
+    var name: String
+    var job: String
+}
+
 struct ContentView: View {
+   var people = [Person(name: "Alphonso", job: "Engineer"),Person(name: "Chelsea", job: "VP"),Person(name: "Danielle", job: "Marketing Manager")]
+    
     var body: some View {
-        Text("Hello World")
+        List(people) { item in
+            HStack {
+                Image("placeholder")
+                VStack {
+                    Text(item.name)
+                    Text(item.job)
+                }
+            }
+            
+        }
     }
 }
 
