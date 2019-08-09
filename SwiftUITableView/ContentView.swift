0c2dd1d142a1d4e472f5071217157eaaf7130e9e
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2019-08-08 18:02:49 -0700

    Initial Commit

---
+++ SwiftUITableView/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITableView
+//
+//  Created by user on 8/8/19.
+//  Copyright © 2019 Alphonso Sensley II. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
